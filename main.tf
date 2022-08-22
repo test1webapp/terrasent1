564fb70bf47699d086e0a242c40823012a7201f5
test1webapp <82330903+test1webapp@users.noreply.github.com>
2022-08-22 14:32:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+provider "aws" {
+  region  = "us-east-2"
+  access_key = var.access_key
+  secret_key = var.secret_key
+ 
+}
 
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t3.micro"
+}
